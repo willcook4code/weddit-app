5be348e938403cc0527b292c72ef953c07f0acdc
Will Cook <wellsee83@gmail.com>
2016-05-04 14:42:04 -0500

    Google Embed works in React

--- public/scripts/components/pages/Attendees.js
+++ public/scripts/components/pages/Attendees.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Iframe from 'react-iframe';
 
 export default React.createClass({
 	render: function() {
 		return(
 			<section className='attendeesPage'>
-				<h1>Attendees</h1>
+				<div className='hotelDisplay'>
+					<h2>Hotels</h2>
+
+				</div>
+				<div className='wideSearchDisplay'>
+					<Iframe
+						url="https://www.google.com/maps/embed/v1/search?key=AIzaSyDORTq8B5-auRcsIQ5pE3lVosmLftZIwmk&q=lodging+and+hotels+near+Inn+At+Wild+Rose+Hall" 
+						width="600"
+						height="450"
+						frameborder="0" style="border:0"
+						allowfullscreen
+					/>
+				</div>
+				<div className='songRequestBox'>
+				</div>
 			</section>
 			);
 	}
